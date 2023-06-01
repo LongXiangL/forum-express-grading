cb1780ec8c8e0f9988a9d8ee7a45e6c3153b7d05
LongXiangL <bfh1104@gmail.com>
2023-06-01 18:36:56 +0800

    feat: add admin-services

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const admin = require('./modules/admin')
 const restController = require('../../controllers/apis/restaurant-controller')
-router.get('/restaurants', restController.getRestaurants)
+
+router.use('/admin', admin)
+router.get('/admin/restaurants', restController.getRestaurants)
 
 module.exports = router
